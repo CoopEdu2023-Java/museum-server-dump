20643d659ecd806d2e13dfd9c832f7374a5cd565
Hikari <xuanzhao@moonshotacademy.cn>
2024-11-27 20:14:49 +0800

    Security settings including custom JWT filter. Global exception handler with code and message.

---
+++ src/main/java/cn/msa/msa_museum_server/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package cn.msa.msa_museum_server.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private Integer code;
+    private String message;
+
+    public BusinessException(ExceptionEnum exceptionEnum){
+        this.code = exceptionEnum.getCode();
+        this.message = exceptionEnum.getMessage();
+        
+    }
+}
---
+++ src/main/java/cn/msa/msa_museum_server/exception/ExceptionEnum.java
@@ -0,0 +1,17 @@
+package cn.msa.msa_museum_server.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ExceptionEnum {
+
+    MISSING_PARAMETERS(2000, "Missing the parameters"),
+    USER_EXISTS(2001, "User already exists"),
+    USER_DOES_NOT_EXIST(2002, "User does not exist"),
+    WRONG_PASSWORD(2003, "Wrong password");
+
+    private final Integer code;
+    private final String message;
+}
