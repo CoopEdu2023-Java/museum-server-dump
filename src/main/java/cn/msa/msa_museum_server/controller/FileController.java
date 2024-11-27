a5ee736792f17e1517c8959cd6347e3eddc2aa93
wwzzhhrr <160588135@qq.com>
2024-11-27 23:40:10 +0800

    添加了在输入异常时的日志

--- src/main/java/cn/msa/msa_museum_server/controller/FileController.java
+++ src/main/java/cn/msa/msa_museum_server/controller/FileController.java
@@ -5,16 +5,18 @@ import cn.msa.msa_museum_server.exception.BusinessException;
 import cn.msa.msa_museum_server.exception.ExceptionEnum;
 import cn.msa.msa_museum_server.entity.FileEntity;
 import cn.msa.msa_museum_server.service.FileService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class FileController {
 
     private final FileService fileService;
@@ -34,13 +36,24 @@ public class FileController {
             int pageSize = Integer.parseInt(size);
 
             if (pageNumber < 0 || pageSize <= 0) {
+                log.warn("Invalid pagination parameters: page={}, size={}", page, size);
                 throw new BusinessException(ExceptionEnum.INVALID_ENTRY);
             }
 
             Pageable pageable = PageRequest.of(pageNumber, pageSize);
             return new ResponseDto<>(0, "", fileService.getFileList(pageable));
         } catch (NumberFormatException e) {
+            log.warn("Pagination parameters are not numbers: page={}, size={}", page, size);
             throw new BusinessException(ExceptionEnum.INVALID_ENTRY_TYPE);
         }
+    }
+
+    @PostMapping("/log")
+    public void log() {
+        log.trace("TRACE");
+        log.debug("DEBUG");
+        log.info("INFO");
+        log.warn("WARN");
+        log.error("ERROR");
     }
 }
