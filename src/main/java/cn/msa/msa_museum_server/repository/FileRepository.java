7c57b4a19c74a958921a6b116109612a8a19347b
meow <winter7z@163.com>
2024-11-28 15:47:55 +0800

    Update ExceptionEnum/FileRepository, delete test (cannot run ;w;)

--- src/main/java/cn/msa/msa_museum_server/repository/FileRepository.java
+++ src/main/java/cn/msa/msa_museum_server/repository/FileRepository.java
@@ -2,8 +2,8 @@ package cn.msa.msa_museum_server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import cn.msa.msa_museum_server.entity.UserEntity;
+import cn.msa.msa_museum_server.entity.FileEntity;
 
-public interface FileRepository extends JpaRepository<UserEntity, Integer> {
+public interface FileRepository extends JpaRepository<FileEntity, Integer> {
 
 }
