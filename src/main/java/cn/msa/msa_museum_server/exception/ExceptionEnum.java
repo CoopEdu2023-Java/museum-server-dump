ba408a58fbad69112a045c298827e64927e5c445
Kros-ZERO <106133818+Kros-ZERO@users.noreply.github.com>
2024-12-03 18:11:58 +0800

    Revert "File Upload API"

--- src/main/java/cn/msa/msa_museum_server/exception/ExceptionEnum.java
+++ src/main/java/cn/msa/msa_museum_server/exception/ExceptionEnum.java
@@ -12,10 +12,7 @@ public enum ExceptionEnum {
     USER_DOES_NOT_EXIST(2002, "User does not exist"),
     WRONG_PASSWORD(2003, "Wrong password"),
     NEW_PASSWORD_SAME_AS_OLD(2004, "Your new password is the same as the old one. Change a different password."),
-    NEW_PASSWORD_UNDER_REQUIREMENTS(2005,
-            "Your new password must have a length of 8 and has at least one of the following: Uppercase Letters [A-Z], lowercase Letters [a-z], numbers [0-9], and symbols[!@~]."),
-    EMPTY_FILE(3001, "Empty file"),
-    UPLOAD_FILE_ERROR(3002, "Upload file error");
+    NEW_PASSWORD_UNDER_REQUIREMENTS(2005, "Your new password must have a length of 8 and has at least one of the following: Uppercase Letters [A-Z], lowercase Letters [a-z], numbers [0-9], and symbols[!@~].");
 
     private final Integer code;
     private final String message;
--- src/main/java/cn/msa/msa_museum_server/repository/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.msa.msa_museum_server.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cn.msa.msa_museum_server.entity.FileEntity;
-
-public interface FileRepository extends JpaRepository<FileEntity, Integer> {
-
-}
