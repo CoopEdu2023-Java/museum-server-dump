e07455078238769ab0a9aeeb3941c403ced25978
vio3500 <vio3500@github>
2024-12-16 12:32:08 +0800

    Testing

--- src/main/java/cn/msa/msa_museum_server/controller/UserController.java
+++ src/main/java/cn/msa/msa_museum_server/controller/UserController.java
@@ -16,26 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-  @Autowired private UserService userService; // 修改变量名，语义更清晰
-  @Autowired private UserRepository userRepository;
+    @Autowired
+    private UserService userService; // 修改变量名，语义更清晰
+    @Autowired
+    private UserRepository userRepository;
 
-  @PostMapping("/login")
-  public ResponseDto<String> login(@RequestBody LoginDto loginDto) {
-    if (loginDto.getUsername() == null || loginDto.getPassword() == null) {
-      throw new BusinessException(ExceptionEnum.MISSING_PARAMETERS);
+    @PostMapping("/login")
+    public ResponseDto<String> login(@RequestBody LoginDto loginDto) {
+        if (loginDto.getUsername() == null || loginDto.getPassword() == null) {
+            throw new BusinessException(ExceptionEnum.MISSING_PARAMETERS);
+        }
+        String token = userService.login(loginDto);
+        return new ResponseDto<String>(token);
     }
-    String token = userService.login(loginDto);
-    return new ResponseDto<String>(token);
-  }
 
-  @PostMapping("/password/reset")
-  public ResponseDto<Void> resetPassword(@RequestBody ResetPasswordDto resetPasswordDto) {
-    if (resetPasswordDto.getUsername() == null
-        || resetPasswordDto.getPassword() == null
-        || resetPasswordDto.getNewPassword() == null) {
-      throw new BusinessException(ExceptionEnum.MISSING_PARAMETERS);
+    @PostMapping("/password/reset")
+    public ResponseDto<Void> resetPassword(@RequestBody ResetPasswordDto resetPasswordDto) {
+        if (resetPasswordDto.getUsername() == null || resetPasswordDto.getPassword() == null || resetPasswordDto.getNewPassword() == null) {
+            throw new BusinessException(ExceptionEnum.MISSING_PARAMETERS);
+        }
+        userService.resetPassword(resetPasswordDto);
+        return new ResponseDto<>();
     }
-    userService.resetPassword(resetPasswordDto);
-    return new ResponseDto<>();
-  }
 }
