490544fd1dab9d73ffe469ccea1f6fed9a2b685b
vio3500 <vio3500@github>
2024-12-16 12:22:07 +0800

    Testing

--- src/main/java/cn/msa/msa_museum_server/entity/UserEntity.java
+++ src/main/java/cn/msa/msa_museum_server/entity/UserEntity.java
@@ -1,19 +1,17 @@
 package cn.msa.msa_museum_server.entity;
 
-import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
 @Data
 @NoArgsConstructor
 public class UserEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @Column(unique = true)
     private String username;
 
