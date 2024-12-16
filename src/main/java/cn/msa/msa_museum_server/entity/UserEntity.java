a0eb3ec665bfa00a696f2b8d1205f8f4c0cf2504
vio3500 <vio3500@github>
2024-12-16 12:23:17 +0800

    Testing

--- src/main/java/cn/msa/msa_museum_server/entity/UserEntity.java
+++ src/main/java/cn/msa/msa_museum_server/entity/UserEntity.java
@@ -8,20 +8,18 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 @Data
 @NoArgsConstructor
-
 public class UserEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
 
+  @Column(unique = true)
+  private String username;
 
-    @Column(unique = true)
-    private String username;
+  private String password;
 
-    private String password;
-
-    public UserEntity(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
+  public UserEntity(String username, String password) {
+    this.username = username;
+    this.password = password;
+  }
 }
