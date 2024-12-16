2e330429ff4af17339a3aa2d0810c7f785868d8d
vio3500 <vio3500@github>
2024-12-16 12:23:24 +0800

    Testing

--- src/main/java/cn/msa/msa_museum_server/controller/UserController.java
+++ src/main/java/cn/msa/msa_museum_server/controller/UserController.java
@@ -1,10 +1,5 @@
 package cn.msa.msa_museum_server.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import cn.msa.msa_museum_server.dto.LoginDto;
 import cn.msa.msa_museum_server.dto.ResetPasswordDto;
 import cn.msa.msa_museum_server.dto.ResponseDto;
@@ -12,30 +7,35 @@ import cn.msa.msa_museum_server.exception.BusinessException;
 import cn.msa.msa_museum_server.exception.ExceptionEnum;
 import cn.msa.msa_museum_server.repository.UserRepository;
 import cn.msa.msa_museum_server.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    @Autowired
-    private UserService userService; // 修改变量名，语义更清晰
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired private UserService userService; // 修改变量名，语义更清晰
+  @Autowired private UserRepository userRepository;
 
-    @PostMapping("/login")
-    public ResponseDto<String> login(@RequestBody LoginDto loginDto) {
-        if (loginDto.getUsername() == null || loginDto.getPassword() == null) {
-            throw new BusinessException(ExceptionEnum.MISSING_PARAMETERS);
-        }
-        String token = userService.login(loginDto);
-        return new ResponseDto<String>(token);
+  @PostMapping("/login")
+  public ResponseDto<String> login(@RequestBody LoginDto loginDto) {
+    if (loginDto.getUsername() == null || loginDto.getPassword() == null) {
+      throw new BusinessException(ExceptionEnum.MISSING_PARAMETERS);
     }
+    String token = userService.login(loginDto);
+    return new ResponseDto<String>(token);
+  }
 
-    @PostMapping("/password/reset")
-    public ResponseDto<Void> resetPassword(@RequestBody ResetPasswordDto resetPasswordDto) {
-        if (resetPasswordDto.getUsername() == null || resetPasswordDto.getPassword() == null || resetPasswordDto.getNewPassword() == null) {
-            throw new BusinessException(ExceptionEnum.MISSING_PARAMETERS);
-        }
-        userService.resetPassword(resetPasswordDto);
-        return new ResponseDto<>();
+  @PostMapping("/password/reset")
+  public ResponseDto<Void> resetPassword(@RequestBody ResetPasswordDto resetPasswordDto) {
+    if (resetPasswordDto.getUsername() == null
+        || resetPasswordDto.getPassword() == null
+        || resetPasswordDto.getNewPassword() == null) {
+      throw new BusinessException(ExceptionEnum.MISSING_PARAMETERS);
     }
+    userService.resetPassword(resetPasswordDto);
+    return new ResponseDto<>();
+  }
 }
